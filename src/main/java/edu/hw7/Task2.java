7830bb929d75493e17a7ab125d724b6cdec65056
Facele5s <otherworldly.elban@gmail.com>
2023-11-22 22:23:09 +0300

    hw7 Task2

---
+++ src/main/java/edu/hw7/Task2.java
@@ -0,0 +1,16 @@
+package edu.hw7;
+
+import java.util.stream.LongStream;
+
+public class Task2 {
+    private Task2() {
+
+    }
+
+    public static long factorialParallel(long n) {
+        return LongStream.rangeClosed(1, n)
+            .parallel()
+            .reduce((a, b) -> a * b)
+            .orElse(1);
+    }
+}
