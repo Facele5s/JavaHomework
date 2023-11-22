3c5681933fcb50b00063c8d1c275cf3647a8cde5
Facele5s <otherworldly.elban@gmail.com>
2023-11-22 19:39:31 +0300

    hw7 Task1

---
+++ src/main/java/edu/hw7/MultiThreadCounter.java
@@ -0,0 +1,34 @@
+package edu.hw7;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MultiThreadCounter {
+    private final static Logger LOGGER = LogManager.getLogger();
+    private final AtomicInteger count = new AtomicInteger(0);
+    private final int n;
+
+    public MultiThreadCounter(int n) {
+        this.n = n;
+    }
+
+    public synchronized void increment() {
+        if (count.get() < n) {
+            count.incrementAndGet();
+            LOGGER.info(Thread.currentThread().getName() + ": " + count.get());
+        }
+    }
+
+    public void startCounting(int threads) {
+        for (int i = 0; i < threads; i++) {
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < n; j++) {
+                    increment();
+                }
+            });
+
+            thread.start();
+        }
+    }
+}
